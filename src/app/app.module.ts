967e862908e3e373d15c0de10a0434abe67bfc79
boxulavm <you@example.com>
2019-10-01 15:51:21 +0200

    'added_animations'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkingAreaComponent } from './working-area/working-area.component';
@@ -14,7 +16,8 @@ import { LoaderComponent } from './loader/loader.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/working-area/working-area.component.ts
+++ src/app/working-area/working-area.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { setTimeout } from 'timers';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-working-area',
   templateUrl: './working-area.component.html',
-  styleUrls: ['./working-area.component.css']
+  styleUrls: ['./working-area.component.css'],
+
+  animations: [
+    trigger('fadeIn', [
+      state('normal', style({
+        opacity: 0
+      })),
+      state('high', style({
+        opacity: 1
+      })),
+      transition('normal <=> high', animate(500))
+    ]),
+    trigger('postsIn', [
+      state('normal', style({
+        opacity: 0
+      })),
+      state('high', style({
+        opacity: 1
+      })),
+      transition('normal => high', animate(1000))
+    ]),
+  ]
+
 })
 export class WorkingAreaComponent implements OnInit {
-
+  state = 'normal';
+  postsState = 'normal';
+  
   loadedPosts = [];
 
   loading = false;
@@ -17,6 +41,14 @@ export class WorkingAreaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.onAnimate()
+    }, 500);;
+  }
+
+
+  private onAnimate(){
+    this.state = 'high';
   }
 
   // Click Event
@@ -31,9 +63,12 @@ export class WorkingAreaComponent implements OnInit {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(posts => {
-        this.loadedPosts = [...posts.slice(0, 7)]
+        this.loadedPosts = [...posts.slice(0, 7)];
         setTimeout(() => {
           this.loading = false;
+          setTimeout(() => {
+            this.postsState = 'high';
+          }, 100);
         }, 1000);
       });
   }
